b0a01a498621d60590e10a5a02eab14c688763ad
MK <maxcowmichel75@gmx.de>
2017-04-05 14:12:33 +0200

    rm some stuff

--- android-build/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/student/Desktop/MMP_1_Spaceinvaders/android-build/bin/AndroidManifest.xml : \
-/home/student/Desktop/MMP_1_Spaceinvaders/android-build/AndroidManifest.xml \
--- android-build/gen/org/qtproject/example/MMP_1_Spaceinvaders/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.qtproject.example.MMP_1_Spaceinvaders;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
